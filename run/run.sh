d4fa175bcf0bbffa03f509c141efdf01a46efe6e
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2024-04-20 23:00:16 +0900

    updated

--- run/run.sh
+++ run/run.sh
@@ -1,4 +1,4 @@
 #python run_ootd.py --model_path ./examples/model/01008_00.jpg --cloth_path ./examples/garment/00055_00.jpg  --scale 2.0 --sample 1
 #python run_ootd.py --model_path ./examples/model/01008_00.jpg --cloth_path ../target_garments/first_garment.jpg  --scale 2.0 --sample 1
 #python run_ootd.py --model_path ../input_images/ichao.png --cloth_path ../target_garments/first_garment.jpg  --scale 2.0 --sample 1
-python run_ootd.py --model_path ../input_images/ichao2.png --cloth_path ../target_garments/first_garment.jpg --model_type dc --category 0 --scale 2.0 --sample 1
+python run_ootd.py --model_path ../input_images/yinfei.png --cloth_path ../target_garments/first_garment.jpg --model_type dc --category 0 --scale 2.0 --sample 1
